895388d1f96f041bbe15533443bcb00a083a14cc
gwenutley <gwen.utley24@gmail.com>
2025-09-10 14:30:31 -0600

    individual activity: add item to cart

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,27 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
-
+/*
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
 }
+*/
+
+function addProductToCart(product) {
+  let cart = getLocalStorage("so-cart") || [];
+  const cartProducts = cart.find((item) => item.Id === product.Id);
+
+  if (cartProducts) {
+    cartProducts.quantity = (cartProducts.quantity || 1) + 1;
+  } else {
+    product.quantity = 1;
+    cart.push(product);
+  }
+
+  setLocalStorage("so-cart", cart);
+}
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
