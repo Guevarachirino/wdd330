b7b2f7fc6d44d4c234280eb6af1a52b62445f49d
Guevarachirino <chi23017@byui.edu>
2025-09-15 17:51:48 -0400

    updatingweektwo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   root: "src/",
 
-  build: {
+  /*build: {
     outDir: "../dist",
     rollupOptions: {
       input: {
@@ -26,5 +26,18 @@ export default defineConfig({
         ),
       },
     },
+  },*/
+  build: {
+  outDir: "../dist",
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, "src/index.html"),
+      cart: resolve(__dirname, "src/cart/index.html"),
+      checkout: resolve(__dirname, "src/checkout/index.html"),
+      product: resolve(__dirname, "src/product_pages/index.html"
+      ),
+
+    },
   },
+},
 });
